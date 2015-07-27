4e9455569a982923042bd8aa080176325f5a79c1
dagenes <hrdcore23@gmail.com>
2015-07-27 16:51:38 +0300

    For the freedom!

---
+++ ConsoleApplication3/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Renci.SshNet;
+using System.IO;
+using System.Threading;
+using System.ComponentModel;
+using System.CodeDom;
+using System.Drawing;
+using System.Windows.Input;
+
+
+
+namespace ConsoleApplication3
+{
+    class Program
+    {
+        static Renci.SshNet.SshClient ssh;
+        static Renci.SshNet.ShellStream stream;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("fd");
+           
+            try
+            {
+                ssh = new SshClient("10.141.3.110", "root", "ismail");
+                ssh.Connect();
+                 //status = true;
+                 //timer_enable();
+            }
+            //catch (Exception ex)
+            // {
+            //    Console.Write(ex.Message);
+            //}
+
+            catch { }
+
+           if (true)
+            try
+            {
+                stream = ssh.CreateShellStream("xterm", 80, 50, 1024, 1024, 1024);
+                Thread.Sleep(100);
+                stream.WriteLine("telnet localhost 6571");
+                Thread.Sleep(100);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("hata");
+            }                    
+            
+            Console.ReadKey();
+        }
+        
+    }
+}
